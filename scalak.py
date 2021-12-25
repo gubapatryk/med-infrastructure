7ff8a6702ca4e4a3868f2be98bf76451788aff77
gubapatryk <gubapatryk@gmail.com>
2021-12-25 16:38:55 +0100

    Created dataset file with header

--- scalak.py
+++ scalak.py
@@ -1,44 +1,53 @@
 import csv
+import os
 
-LICZBA_CECH_W_PLIKU = 4
+firstIteration = True
 
 slowik = {}
 
-ilePol = 0
-currentListName = 'nun'
-with open('drg.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
-    for row in spamreader:
-        for field in row:
-            if ilePol > 0:
-                slowik[currentListName].append(field)
-                ilePol = ilePol - 1
-            if field.startswith('\"Powiat'):
-                currentListName = field
-                ilePol = LICZBA_CECH_W_PLIKU
-                if currentListName in slowik:
-                    print('e')
-                else:
-                    slowik[currentListName] = []
+directory = r'./dane'
+for filename in os.listdir(directory):
+    filepath = directory + '/' + filename
+
+    file = open(filepath)
+    content = file.readlines()
+
+    LICZBA_CECH_W_PLIKU = content[1].count(';') - 2
+
+    ilePol = 0
+    currentListName = 'nun'
+    with open(filepath, newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
+        for row in spamreader:
+            for field in row:
+                if ilePol > 0:
+                    slowik[currentListName].append(field)
+                    ilePol = ilePol - 1
+                if field.startswith('\"Powiat'):
+                    currentListName = field
+                    ilePol = LICZBA_CECH_W_PLIKU
+                    if firstIteration:
+                        if not currentListName in slowik:
+                            slowik[currentListName] = [currentListName]
+    firstIteration = False
+
+#print(slowik)
+
+lista = []
+id = 1
+
+for val in slowik.values():
+    val.insert(0,id)
+    lista.append(val)
+    id = id + 1
 
 
-LICZBA_CECH_W_PLIKU = 1
+with open("dane.csv", "w", newline="") as f:
+    for i in range(len(lista)):
+        for k in range(len(lista[i])):
+            f.write(str(lista[i][k]) + ',')
+        f.write('\n')
 
-ilePol = 0
-currentListName = 'nun'
-with open('moni.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
-    for row in spamreader:
-        for field in row:
-            if ilePol > 0:
-                slowik[currentListName].append(field)
-                ilePol = ilePol - 1
-            if field.startswith('\"Powiat'):
-                currentListName = field
-                ilePol = LICZBA_CECH_W_PLIKU
-                if currentListName in slowik:
-                    print('e')
-                else:
-                    slowik[currentListName] = []
 
-print(slowik)
+for filename in os.listdir(directory):
+    print(filename)
